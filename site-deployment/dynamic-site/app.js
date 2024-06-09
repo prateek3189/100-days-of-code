9bb07b8e99278e99702335021ced092b02e8a238
prateek3189 <prateek.magarde@gmail.com>
2024-06-09 14:16:52 +0530

    Heroku

--- site-deployment/dynamic-site/app.js
+++ site-deployment/dynamic-site/app.js
@@ -1,28 +1,33 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/database');
-const mainRoutes = require('./routes/main.routes');
+const db = require("./data/database");
+const mainRoutes = require("./routes/main.routes");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 // Could add more middleware - e.g. session, body parsers etc.
 
 app.use(mainRoutes);
 
-app.use(function(error, req, res, next) {
-  res.status(500).render('500');
-})
+app.use(function (error, req, res, next) {
+  res.status(500).render("500");
+});
+let PORT = 3000;
+
+if (process.env.PORT) {
+  PORT = process.env.PORT;
+}
 
 db.initDatabase()
   .then(function () {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch(function (error) {
-    console.log('Connecting to the database failed!');
+    console.log("Connecting to the database failed!");
   });
--- site-deployment/dynamic-site/data/database.js
+++ site-deployment/dynamic-site/data/database.js
@@ -5,7 +5,13 @@ const MongoClient = mongodb.MongoClient;
 let database;
 
 async function initDatabase() {
-  const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
+  // "mongodb+srv://Prateek:<>@cluster0.143rbfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  let mongodbUrl = "mongodb://127.0.0.1:27017";
+  if (process.env.MONGODB_URL) {
+    mongodbUrl = process.env.MONGODB_URL;
+  }
+
+  const client = await MongoClient.connect(mongodbUrl);
   database = client.db("deployment");
 }
 
